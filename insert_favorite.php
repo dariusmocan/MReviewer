0da6262878623bcdc587057dc26def2de51e425d
dariusmocan <93878729+dariusmocan@users.noreply.github.com>
2024-01-15 03:35:08 +0200

    Update insert_favorite.php

--- insert_favorite.php
+++ insert_favorite.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 include 'components/connect.php';
 
 $response = array();
@@ -12,62 +12,42 @@ if ($user_id != '') {
     $overview = isset($_POST['overview']) ? $_POST['overview'] : '';
     $voteAverage = isset($_POST['vote_average']) ? $_POST['vote_average'] : '';
 
-
     $verify_fav = $conn->prepare("SELECT * FROM `favorite` WHERE id = ? AND user_id = ?");
     $verify_fav->execute([$id, $user_id]);
 
-
-    if($verify_fav->rowCount() > 0){
-        $warning_msg[] = 'The movie is already added to your favorites';
-
-     }else{
-    // Insert data into the 'favorite' table
-    $sql = "INSERT INTO `favorite` (id, title, image, overview, voteAverage, user_id) VALUES (?, ?, ?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-
-    if ($stmt) {
-        $stmt->bindParam(1, $id);
-        $stmt->bindParam(2, $title);
-        $stmt->bindParam(3, $image);
-        $stmt->bindParam(4, $overview);
-        $stmt->bindParam(5, $voteAverage);
-        $stmt->bindParam(6, $user_id);
-
-        if ($stmt->execute()) {
-            // Redirect to the desired page
-            //header('Location: all_posts.php');
-            //exit();
-            $success_msg[] = 'Movie added to favorites';
-            
-        } else {
-            $warning_msg[] = 'Please login first!';
-        }
+    if ($verify_fav->rowCount() > 0) {
+        $response['success'] = false;
+        $response['message'] = 'The movie is already added to your favorites';
     } else {
-        $warning_msg[] = 'Please login first!';
+        // Insert data into the 'favorite' table
+        $sql = "INSERT INTO `favorite` (id, title, image, overview, voteAverage, user_id) VALUES (?, ?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+
+        if ($stmt) {
+            $stmt->bindParam(1, $id);
+            $stmt->bindParam(2, $title);
+            $stmt->bindParam(3, $image);
+            $stmt->bindParam(4, $overview);
+            $stmt->bindParam(5, $voteAverage);
+            $stmt->bindParam(6, $user_id);
+
+            if ($stmt->execute()) {
+                $response['success'] = true;
+                $response['message'] = 'Movie added to favorites';
+            } else {
+                $response['success'] = false;
+                $response['message'] = 'Please login first!';
+            }
+        } else {
+            $response['success'] = false;
+            $response['message'] = 'Please login first!';
+        }
     }
-}
 } else {
-    $warning_msg[] = 'Please login first!';
+    $response['success'] = false;
+    $response['message'] = 'Please login first!';
 }
 
+header('Content-Type: application/json');
+echo json_encode($response);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
-</head>
-<body>
-    
-<script src="js/script2.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
-
-<!-- custom js file link  -->
-<script src="js/script.js"></script>
-<?php include 'components/alers.php'; ?>
-</body>
-</html>
