46d0e9790d78420e47facb118b016737c665580f
dariusmocan <93878729+dariusmocan@users.noreply.github.com>
2024-01-15 03:36:43 +0200

    Update view_post.php

--- view_post.php
+++ view_post.php
@@ -30,7 +30,7 @@ if(isset($_POST['delete_review'])){
    <title>view post</title>
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="postscript.css">
 
 </head>
 <body>
@@ -70,72 +70,63 @@ if(isset($_POST['delete_review'])){
 <!-- view posts section ends -->
 
 <!-- reviews section starts  -->
-
 <section class="reviews-container">
-
-   <div class="heading"><h1>user's reviews</h1> <a href="add_review.php?id=<?= $postId ?>&title=<?= urlencode($title) ?>&overview=<?= urlencode($overview) ?>&image=<?= urlencode($image) ?>&vote_average=<?= $voteAverage ?>" class="inline-option-btn" style="margin-top: 0;">add review</a></div>
-
+   <div class="heading">
+      <h1>user's reviews</h1>
+      <a href="add_review.php?id=<?= $postId ?>&title=<?= urlencode($title) ?>&overview=<?= urlencode($overview) ?>&image=<?= urlencode($image) ?>&vote_average=<?= $voteAverage ?>" class="inline-option-btn" style="margin-top: 0;">add review</a>
+   </div>
    <div class="box-container">
-   <?php
+      <?php
       $select_reviews = $conn->prepare("SELECT * FROM `reviews` WHERE post_id = ?");
       $select_reviews->execute([$postId]);
-      if($select_reviews->rowCount() > 0){
-         while($fetch_review = $select_reviews->fetch(PDO::FETCH_ASSOC)){
-   ?>
-   <div class="box" <?php if($fetch_review['user_id'] == $user_id){echo 'style="order: -1;"';}; ?>>
+      if ($select_reviews->rowCount() > 0) {
+         while ($fetch_review = $select_reviews->fetch(PDO::FETCH_ASSOC)) {
+            ?>
+            <div class="box" <?php if ($fetch_review['user_id'] == $user_id) {
+                                 echo 'style="order: -1;"';
+                              }; ?>>
+               <?php
+               $select_user = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+               $select_user->execute([$fetch_review['user_id']]);
+               while ($fetch_user = $select_user->fetch(PDO::FETCH_ASSOC)) {
+               ?>
+                  <div class="user">
+                     <?php if ($fetch_user['image'] != '') { ?>
+                        <img src="uploaded_files/<?= $fetch_user['image']; ?>" alt="">
+                     <?php } else { ?>
+                        <h3><?= substr($fetch_user['name'], 0, 1); ?></h3>
+                     <?php }; ?>
+                     <div>
+                        <p><?= $fetch_user['name']; ?></p>
+                        <span><?= $fetch_review['date']; ?></span>
+                     </div>
+                  </div>
+               <?php }; ?>
+               <h3 class="title"><?= $fetch_review['title']; ?></h3>
+               <?php if ($fetch_review['description'] != '') { ?>
+                  <p class="description"><?= $fetch_review['description']; ?></p>
+               <?php }; ?>
+               <?php if ($fetch_review['rating'] != '') { ?>
+                  <div class="rating">
+                     <?= $fetch_review['rating']; ?>
+                  </div>
+               <?php }; ?>
+               <?php if ($fetch_review['user_id'] == $user_id) { ?>
+                  <form action="" method="post" class="flex-btn" style="justify-content: flex-end;">
+                     <input type="hidden" name="delete_id" value="<?= $fetch_review['id']; ?>">
+                     <input type="submit" value="delete review" class="know-more" name="delete_review" onclick="return confirm('delete this review?');">
+                  </form>
+               <?php }; ?>
+            </div>
       <?php
-         $select_user = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
-         $select_user->execute([$fetch_review['user_id']]);
-         while($fetch_user = $select_user->fetch(PDO::FETCH_ASSOC)){
-      ?>
-      <div class="user">
-         <?php if($fetch_user['image'] != ''){ ?>
-            <img src="uploaded_files/<?= $fetch_user['image']; ?>" alt="">
-         <?php }else{ ?>   
-            <h3><?= substr($fetch_user['name'], 0, 1); ?></h3>
-         <?php }; ?>   
-         <div>
-            <p><?= $fetch_user['name']; ?></p>
-            <span><?= $fetch_review['date']; ?></span>
-         </div>
-      </div>
-      <?php }; ?>
-      
-      <h3 class="title"><?= $fetch_review['title']; ?></h3>
-      <?php if($fetch_review['description'] != ''){ ?>
-         <p class="description"><?= $fetch_review['description']; ?></p>
-      <?php }; ?>  
-      <?php if($fetch_review['user_id'] == $user_id){ ?>
-         <form action="" method="post" class="flex-btn" style="justify-content: flex-end;">
-            <input type="hidden" name="delete_id" value="<?= $fetch_review['id']; ?>">
-            <!--<a href="update_review.php?get_id= <?= $fetch_review['id']; ?>" class="inline-option-btn">edit review</a>-->
-            <input type="submit" value="delete review" class="inline-delete-btn" name="delete_review" onclick="return confirm('delete this review?');">
-</form>
-      <?php }; ?>   
-   </div>
-   <?php
          }
-      }else{
+      } else {
          echo '<p class="empty">no reviews added yet!</p>';
       }
-   ?>
-
+      ?>
    </div>
-   
-
 </section>
-
 <!-- reviews section ends -->
-
-
-
-
-
-
-
-
-
-
 
 
 
