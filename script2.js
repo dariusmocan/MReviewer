c5b884714ab6161e91ae405a3041fe9aac513d36
DariusMocan <93878729+dariusmocan@users.noreply.github.com>
2024-03-20 16:51:51 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,11 +1,5 @@
 //TMDB 
 
-const API_KEY = 'api_key=560c5dcf3b6a2bf2da60e15e62f86817';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?'+API_KEY;
-
 const genres = [
     {
       "id": 28,
